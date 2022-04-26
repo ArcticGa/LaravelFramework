acf7328e19271c3eaa8871f1bcad39c9fdbc46a7
is42-zotovas <zotov009@mail.ru>
2022-04-26 10:28:58 +0500

    26.04 coomit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('my-route', [TestController::class, 'lessonOne']);
+Route::get('data', [TestController::class, 'lessonTwo']);
+Route::get('lovechrt', [TestController::class, 'loveChrt']);
+Route::get('name', [TestController::class, 'nameIvan']);
+Route::get('nameandfruit', [TestController::class, 'nameAndFruits']);
+Route::get('suminprog', [TestController::class, 'sumInProg']);
+Route::get('summa', [TestController::class, 'sum']);
+Route::get('cipher', [TestController::class, 'cipher']);
